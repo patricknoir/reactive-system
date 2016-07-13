e35fbb3fb2b0c92ac8a7d12f76cc60f99fb911d3
Patrick Di Loreto <diloreto.p@gmail.com>
2016-07-13 06:07:46 +0200

    move reactive-route in a different file

--- src/main/scala/org/patricknoir/kafka/reactive/server/ReactiveService.scala
+++ src/main/scala/org/patricknoir/kafka/reactive/server/ReactiveService.scala
@@ -19,23 +19,3 @@ case class ReactiveService[-In: Decoder, +Out: Encoder](id: String)(f: In => Fut
 object ReactiveService {
   implicit def reactiveServiceToRoute(service: ReactiveService[_, _]) = ReactiveRoute().add(service)
 }
-
-case class ReactiveRoute(services: Map[String, ReactiveService[_, _]] = Map.empty[String, ReactiveService[_, _]]) {
-  def add[In, Out](reactiveService: ReactiveService[In, Out]): ReactiveRoute =
-    this.copy(services = services + (reactiveService.id -> reactiveService))
-  def ~(reactiveRoute: ReactiveRoute) = ReactiveRoute(this.services ++ reactiveRoute.services)
-}
-
-object ReactiveRoute {
-
-  //TODO: Replace this with the 'Magnet' Pattern
-
-  def request[In: Decoder, Out: Encoder](id: String)(f: In => Future[Error Xor Out]): ReactiveRoute =
-    ReactiveRoute().add(ReactiveService[In, Out](id)(f))
-
-  def request[In: Decoder, Out: Encoder](id: String)(f: In => Future[Out])(implicit ec: ExecutionContext): ReactiveRoute =
-    ReactiveRoute().add(ReactiveService[In, Out](id)(in => f(in).map(out => Xor.right[Error, Out](out))))
-
-  def requestFuture[In: Decoder, Out: Encoder](id: String)(f: In => Out)(implicit ec: ExecutionContext): ReactiveRoute =
-    ReactiveRoute().add(ReactiveService[In, Out](id) { in => Future(Xor.right(f(in))) })
-}

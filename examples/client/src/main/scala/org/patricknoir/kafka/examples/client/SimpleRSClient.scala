fc3ac1cf30dad4c84b3b73c1a0d3c855069ec8c1
Michal Losiewicz <michal.losiewicz@grandparade.co.uk>
2016-09-21 16:35:05 +0200

    removed var from the client implementation

--- examples/client/src/main/scala/org/patricknoir/kafka/examples/client/SimpleRSClient.scala
+++ examples/client/src/main/scala/org/patricknoir/kafka/examples/client/SimpleRSClient.scala
@@ -5,13 +5,13 @@ import akka.util.Timeout
 import cats.data.Xor
 import org.patricknoir.kafka.reactive.client.KafkaReactiveClient
 import org.patricknoir.kafka.reactive.client.config.KafkaRClientSettings
-import scala.concurrent.{ Await, Future }
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 import scala.io.StdIn
 
 /**
- * Created by patrick on 09/08/2016.
- */
+  * Created by patrick on 09/08/2016.
+  */
 object SimpleRSClient extends App {
 
   implicit val system = ActorSystem("ReactiveClient")
@@ -21,18 +21,15 @@ object SimpleRSClient extends App {
 
   val client = new KafkaReactiveClient(KafkaRClientSettings.default)
 
-  var repeat = true
-
-  while (repeat) {
+  do {
     val response: Future[Error Xor String] = client.request[String, String]("kafka:simple/echo", "hello world!")
 
-    response.onComplete(println)
+    response.onComplete{r => println(r)
+      println("press e + enter to exit, otherwise enter to repeat the request!")
+    }
 
     Await.ready(response, Duration.Inf)
-    println("press e + enter to exit, otherwise enter to repeat the request!")
-    if (StdIn.readLine() == "e")
-      repeat = false
-  }
+  } while (StdIn.readLine() != "e")
 
   Await.ready(system.terminate(), Duration.Inf)
   println("program terminated")
